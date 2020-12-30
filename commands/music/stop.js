19351b3d9ba1bd43cfba8e4170c8140f60a77c38
LordFarquhar <65400619+LordFarquhar@users.noreply.github.com>
2020-12-30 13:36:53 +0000

    fis stop when nothing playing

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -1,6 +1,7 @@
 module.exports.run = async(bot, message, args, dbGuild) => {
     const serverQueue = bot.audioQueue.get(message.guild.id);
     if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to stop the music!');
+    if (!serverQueue) { return message.channel.send("There is nothing to stop playing")}
     serverQueue.songs = [];
     serverQueue.connection.dispatcher.end();
 }
