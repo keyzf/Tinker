30a7438c8ec6f68e832991d0d8aec2f8cc33c42d
LordFarquhar <65400619+LordFarquhar@users.noreply.github.com>
2021-03-05 09:48:52 +0000

    patch vote error returning null

--- commands/vote.js
+++ commands/vote.js
@@ -20,7 +20,7 @@ command.setPerms({
 });
 
 command.setExecute(async(client, message, args, cmd) => {
-    const {voted} = await client.voteManager.getVote(message.author.id);
+    const userVote = await client.voteManager.getVote(message.author.id);
     return message.channel.send(client.operations.generateEmbed.run({
         title: "Voting",
         description: `Voting can provide you with benefits in the future... for now it just helps us gain more publicity! You can [vote here](${client.config.config.voteLink})`,
@@ -28,7 +28,7 @@ command.setExecute(async(client, message, args, cmd) => {
             { name: "Voting Benefit 1", value: "It does this cool stuff", inline: true },
             { name: "Voting Benefit 2", value: "It does this cool stuff as well, I know so much!", inline: true }
         ],
-        footerText: `${voted == null ? "Error getting your vote status" : voted ? "You have already voted, please vote again in 12 hours" : "You have not yet voted today" }`,
+        footerText: `${userVote == null ? "Error getting your vote status" : userVote.voted ? "You have already voted, please vote again in 12 hours" : "You have not yet voted today" }`,
         footerUrl: message.author.displayAvatarURL(),
         colour: client.statics.colours.tinker
     }));
