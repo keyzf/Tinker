963683445c82a318948d0f7edf2c923e53121f81
LordFarquhar <65400619+LordFarquhar@users.noreply.github.com>
2021-05-01 09:03:27 +0100

    another minor view patch

--- commands/uptime.js
+++ commands/uptime.js
@@ -26,7 +26,7 @@ command.setExecute(async(client, message, args, cmd) => {
     try {
         const [{ totalUptime }] = await client.data.db.query(`select totalUptime from bot where env='${process.env.NODE_ENV}'`);
         await message.channel.send(client.operations.generateEmbed.run({
-            description: `Since last restart: ${ms(client.uptime)}\nFrom the very beginning of its existence: ${ms(new Date(totalUptime).getTime())}`,
+            description: `Since last restart: ${ms(client.uptime)}\nFrom the very beginning of its existence: ${ms(new Date(totalUptime + client.uptime).getTime())}`,
             colour: client.statics.colours.tinker,
             ...client.statics.defaultEmbed.footerUser("Requested by", message.author, "")
         }));
